e77c80ab90599283b85bf3727dc40c17c85c4bfa
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-09-13 13:49:29 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-document.getElementById('download-btn').addEventListener('click', () => {
+document.getElementById('copy-btn').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { action: "getContent" }, (response) => {
             chrome.runtime.sendMessage({
-                action: "downloadMarkdown",
+                action: "copyMarkdown",
                 content: response.content,
                 filename: 'example.md'
             });

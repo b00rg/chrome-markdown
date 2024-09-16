b2c6ba03174c0ffeb83ab8a8b03614ade2c5e312
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-09-16 16:19:06 +0100

    correcting indentation

--- background.js
+++ background.js
@@ -1,33 +1,34 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-if (request.action === "getContent") {
-    const selectedText = window.getSelection().toString();
-
-    if (selectedText) {
-        const textarea = document.createElement('textarea');
-        textarea.value = selectedText;
-        document.body.appendChild(textarea);
-        
-        textarea.select();
-        textarea.setSelectionRange(0, textarea.value.length); // For mobile compatibility
-
-        try {
-            document.execCommand('copy');
-            console.log('Selected text copied to clipboard!');
-        } catch (err) {
-            console.error('Failed to copy selected text to clipboard', err);
+    if (request.action === "getContent") {
+        const selectedText = window.getSelection().toString();
+    
+        if (selectedText) {
+            const textarea = document.createElement('textarea');
+            textarea.value = selectedText;
+            document.body.appendChild(textarea);
+            
+            textarea.select();
+            textarea.setSelectionRange(0, textarea.value.length); // For mobile compatibility
+    
+            try {
+                document.execCommand('copy');
+                console.log('Selected text copied to clipboard!');
+            } catch (err) {
+                console.error('Failed to copy selected text to clipboard', err);
+            }
+    
+            document.body.removeChild(textarea);
+        } else {
+            console.log('No text selected to copy.');
         }
-
-        document.body.removeChild(textarea);
-    } else {
-        console.log('No text selected to copy.');
     }
-}
-
+    
 });
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({ text: "OFF" });
+    chrome.action.setBadgeText({ text: "OFF" });
 });
-
+    
 chrome.action.onClicked.addListener(async (tab) => {
     const extensions = 'https://developer.chrome.com/docs/extensions';
     const webstore = 'https://developer.chrome.com/docs/webstore';
@@ -38,5 +39,7 @@ chrome.action.onClicked.addListener(async (tab) => {
         tabId: tab.id,
         text: nextState,
         });
-      }
+        }
 });
+
+    

cf5fa1b6fac3306f5f8a6175ac0f94dffb0250ba
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-09-16 16:01:23 +0100

    Update background.js

--- background.js
+++ background.js
@@ -32,12 +32,8 @@ chrome.action.onClicked.addListener(async (tab) => {
     const extensions = 'https://developer.chrome.com/docs/extensions';
     const webstore = 'https://developer.chrome.com/docs/webstore';
     if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
         const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        // Next state will always be the opposite
         const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-    
-        // Set the action badge to the next state
         await chrome.action.setBadgeText({
         tabId: tab.id,
         text: nextState,

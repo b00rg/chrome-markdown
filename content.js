e58cc1da3aef48b93038a9063fc5fb32ea7b70c4
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-09-16 16:50:39 +0100

    Update content.js for more comprehensive html to markdown conversion

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 #### ${document.querySelector('h4') ? document.querySelector('h4').innerText : 'No Heading'}
 ##### ${document.querySelector('h5') ? document.querySelector('h5').innerText : 'No Heading'}
 ###### ${document.querySelector('h6') ? document.querySelector('h6').innerText : 'No Heading'}
-${document.querySelector('paragraph') ? document.querySelector('paragraph').innerText : 'No Paragraph'}
+${document.querySelector('p') ? document.querySelector('p').innerText : 'No Paragraph'}
 > ${document.querySelector('blockquote') ? document.querySelector('blockquote').innerText : 'No Blockquote'}
 ``` ${document.querySelector('code') ? document.querySelector('code').innerText : 'No Code'} ```
 --- ${document.querySelector('hr') ? document.querySelector('hr').innerText : 'No Horizontal Break'}
@@ -17,8 +17,9 @@ ${document.querySelector('paragraph') ? document.querySelector('paragraph').inne
 * ${document.querySelector('q') ? document.querySelector('q').innerText : 'No Quote'} *
 ~~${document.querySelector('s') ? document.querySelector('s').innerText : 'No Strikethrough'} ~~
 ``` ${document.querySelector('script') ? document.querySelector('script').innerText : 'No Script'} ```
-** ${document.querySelector('strong') ? document.querySelector('strong').innerText : 'No Strong'} ** `
-            ;
-        sendResponse({ content });
+** ${document.querySelector('strong') ? document.querySelector('strong').innerText : 'No Strong'} ** 
+</sub> ${document.querySelector('sub') ? document.querySelector('sub').innerText : 'No Sub'} </sub> 
+</sup> ${document.querySelector('sup') ? document.querySelector('sup').innerText : 'No Sup'} </sup> ;
+    '   sendResponse({ content });
     }
 });

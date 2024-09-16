2c9c967a24c1104112419dfd78f13713f3da19fe
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-09-16 15:34:25 +0100

    Update content.js for headings translation

--- content.js
+++ content.js
@@ -4,7 +4,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         // Extract content from the webpage
         let content = `
 # ${document.querySelector('h1') ? document.querySelector('h1').innerText : 'No Heading'}
-
+## ${document.querySelector('h2') ? document.querySelector('h2').innerText : 'No Heading'}
+### ${document.querySelector('h3') ? document.querySelector('h3').innerText : 'No Heading'}  
+#### ${document.querySelector('h4') ? document.querySelector('h4').innerText : 'No Heading'}
+##### ${document.querySelector('h5') ? document.querySelector('h5').innerText : 'No Heading'}
+###### ${document.querySelector('h6') ? document.querySelector('h6').innerText : 'No Heading'}
 ${document.querySelector('p') ? document.querySelector('p').innerText : 'No Paragraph'}
         `;
         sendResponse({ content });
